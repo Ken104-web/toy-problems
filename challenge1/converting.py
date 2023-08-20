3147cdfe59077ba19dfc2ead31026afe35a3f629
KenCorp <kennethwarui35@gmail.com>
2023-08-20 10:40:31 +0300

    challege1 fixed

--- challenge1/converting.py
+++ challenge1/converting.py
@@ -1,14 +1,15 @@
 # Convert a 12-hour clock system to 24-hour clock-system
 # make a clock format (hh: mm  AM or PM) as input in 12 hour clock system
 #  output in 24-hour clock system
-hour = 18
-minute = 24
-period = "PM"
+hour = int(input("Enter the hour(1-12): "))
+minute = int(input("Enter minute(0-59): "))
+period = input(input("Enter period(am or pm): "))
 
 def convert(hour, minute, period):
-    if period == "PM":
+    if period == "pm" and hour != 12:
         hour += 12
-    hour %= 24
+    elif period == "am" and hour ==12:
+        hour = 0
     return f"{hour:02d}:{minute:02d}"
 result = convert(hour, minute, period)
 print(result)
