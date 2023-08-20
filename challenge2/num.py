0b4be74133110e407951b8a0ed2ea56e26197b65
KenCorp <kennethwarui35@gmail.com>
2023-08-20 10:06:28 +0300

    Completed challenge 2

---
+++ challenge2/num.py
@@ -0,0 +1,16 @@
+#  Write intergers
+# take three intergers as argunments
+def integers(a, b, c):
+    count = 0
+    if a > 0:
+        count += 1
+    if b > 0:
+        count += 1
+    if c > 0:
+        count +=1
+    return count == 2
+
+print(integers(2, 4, -3))
+print (integers(-4, 6, 8))
+print (integers(-4, 6, 0))
+   
